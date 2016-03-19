0d4cdccb68fcb7bc1db62d68ab7619f2c35ff082
Peter Rule <peterrule27@gmail.com>
2016-03-19 18:55:42 +0000

    intro screen done

---
+++ Wells-Next-The-Sea/CustomBtn.swift
@@ -0,0 +1,16 @@
+//
+//  CustomBtn.swift
+//  Wells-Next-The-Sea
+//
+//  Created by Peter Rule on 19/03/2016.
+//  Copyright © 2016 Peter Rule. All rights reserved.
+//
+
+import UIKit
+
+class CustomBtn: UIButton {
+
+    override func awakeFromNib() {
+        layer.cornerRadius = 5.0
+    }
+}
