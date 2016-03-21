232c23ae7852b7b0a2bb52ff161f4cd209b82887
Peter Rule <peterrule27@gmail.com>
2016-03-21 15:56:19 +0000

    phone portrait nearly there

---
+++ Wells-Next-The-Sea/CustomLbl.swift
@@ -0,0 +1,16 @@
+//
+//  CustomLbl.swift
+//  Wells-Next-The-Sea
+//
+//  Created by Peter Rule on 21/03/2016.
+//  Copyright © 2016 Peter Rule. All rights reserved.
+//
+
+import UIKit
+
+class CustomLbl: UILabel {
+    override func awakeFromNib() {
+        textColor = UIColor(red: 142.0/255.0, green: 159.0/255.0, blue: 209.0/255.0, alpha: 1.0)
+        font = UIFont(name: "HelveticaNeue-Bold", size: 16.0)
+    }
+}
