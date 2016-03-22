687311499053a26705b7f0bd2539effd7db17b72
Peter Rule <peterrule27@gmail.com>
2016-03-22 08:40:43 +0000

    label classes done

---
+++ Wells-Next-The-Sea/LabelTitle.swift
@@ -0,0 +1,18 @@
+//
+//  LabelTitle.swift
+//  Wells-Next-The-Sea
+//
+//  Created by Peter Rule on 22/03/2016.
+//  Copyright © 2016 Peter Rule. All rights reserved.
+//
+
+import UIKit
+
+class LabelTitle: UILabel {
+
+    override func awakeFromNib() {
+        textColor = UIColor(red: 142.0/255.0, green: 159.0/255.0, blue: 209.0/255.0, alpha: 1.0)
+        font = UIFont(name: "HelveticaNeue-Bold", size: 15.0)
+    }
+
+}
